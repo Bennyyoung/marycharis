1399e073fbc7f89b621b823c775cbb47a5380116
Decagon <decagon@Decagons-MacBook-Air-6.local>
2022-05-20 08:37:03 +0100

    Added the trade fair image to the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Banner, AidTracker, CardBlock } from '../components/layouts'
 import HomeMedia from './HomeMedia'
+import trade_fair from '../data/img/flair_fair_mcbf.jpeg';
 import TestimonialReview from '../components/TestimonialReview'
 const Home = () => {
   return (
     <main>
-      <Banner />
+      <img src={trade_fair} />
+      {/* <Banner /> */}
       <AidTracker />
       {/* <Campaign /> */}
       <CardBlock />
