e73b8e0dd3cb1e81a5ee8637f9afabe9239e1f23
monsaw <benjamineffiong124@yahoo.com>
2023-02-14 23:17:28 +0100

    Added the Upcoming activity on the foundation with Marquee npm package

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,15 @@ import HomeMedia from './HomeMedia'
 import trade_fair from '../data/img/flair_fair_mcbf.jpeg';
 import home_img from '../data/img/home_img.jpeg';
 import TestimonialReview from '../components/TestimonialReview'
+import Marquee from "react-fast-marquee";
+
 const Home = () => {
   return (
     <main>
-      <div style={{marginTop: "3rem"}}>
-
-      <img src={home_img} />
+      <div style={{ marginTop: "3rem" }}>
+        <Marquee>
+          <h3>The coming soon of the Affable summit 2023</h3>
+        </Marquee>
       </div>
       {/* <Banner /> */}
       <AidTracker />
@@ -17,7 +20,7 @@ const Home = () => {
       <CardBlock />
       {/* <TestimonialReview /> */}
       <HomeMedia />
-      
+
       {/* <Quote /> */}
     </main>
   );
